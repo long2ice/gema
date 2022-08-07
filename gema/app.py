7ccb45a26cf827ee3f68342a7c904d52db0e77ef
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-07 20:32:39 +0800

    feat: remove static

--- gema/app.py
+++ gema/app.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from starlette.requests import Request
-from starlette.staticfiles import StaticFiles
-from starlette.status import HTTP_404_NOT_FOUND
-from starlette.templating import Jinja2Templates
-from fastapi.exception_handlers import http_exception_handler as exception_handler
-from starlette.exceptions import HTTPException as StarletteHTTPException
+
 from gema.api import router
 from gema.exceptions import http_exception_handler
 
@@ -21,14 +16,3 @@ app.add_middleware(
     allow_credentials=True,
 )
 app.add_exception_handler(HTTPException, http_exception_handler)
-
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
-templates = Jinja2Templates(directory="static")
-
-
-@app.exception_handler(StarletteHTTPException)
-async def spa_server(request: Request, exc: StarletteHTTPException):
-    if exc.status_code == HTTP_404_NOT_FOUND:
-        return templates.TemplateResponse("index.html", context={"request": request})
-    else:
-        return await exception_handler(request, exc)
