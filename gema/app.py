d2261c48b77babf350be9dac9525ad836dbde2ed
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-07 20:59:05 +0800

    feat: remove cors

--- gema/app.py
+++ gema/app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from starlette.middleware.cors import CORSMiddleware
 
 from gema.api import router
 from gema.exceptions import http_exception_handler
@@ -8,11 +7,4 @@ app = FastAPI(
     title="Gema", description="Convert from json/xml to Pydantic/Go/Rust etc."
 )
 app.include_router(router)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
-)
 app.add_exception_handler(HTTPException, http_exception_handler)
