aed7511b148f7d430816849b3b80331b17e36f36
long2ice <long2ice@gmail.com>
2022-10-04 17:43:30 +0800

    Revert "feat: remove static"
    This reverts commit 7ccb45a26cf827ee3f68342a7c904d52db0e77ef.

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,16 @@ COPY pyproject.toml poetry.lock /gema/
 ENV POETRY_VIRTUALENVS_CREATE false
 RUN pip3 install poetry && poetry install
 
+FROM node as web-builder
+ENV REACT_APP_API_URL=/
+RUN mkdir -p /src
+WORKDIR /src
+RUN git clone https://github.com/long2ice/gema-web.git
+RUN cd gema-web && npm i && npm run build
+
 FROM python:3.9-slim
 WORKDIR /gema
 COPY --from=builder /usr/local/lib/python3.9/site-packages /usr/local/lib/python3.9/site-packages
 COPY --from=builder /usr/local/bin/ /usr/local/bin/
 COPY . /gema
+COPY --from=web-builder /src/gema-web/build /gema/static
--- gema/app.py
+++ gema/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.status import HTTP_404_NOT_FOUND
+from starlette.templating import Jinja2Templates
+from fastapi.exception_handlers import http_exception_handler as exception_handler
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from gema.api import router
 from gema.exceptions import http_exception_handler
 
@@ -18,3 +23,14 @@ app.add_middleware(
 
 app.include_router(router)
 app.add_exception_handler(HTTPException, http_exception_handler)
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+templates = Jinja2Templates(directory="static")
+
+
+@app.exception_handler(StarletteHTTPException)
+async def spa_server(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == HTTP_404_NOT_FOUND:
+        return templates.TemplateResponse("index.html", context={"request": request})
+    else:
+        return await exception_handler(request, exc)
